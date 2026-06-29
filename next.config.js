e32efece9403ceca687fd55800bd6eea1e7eef27
nihalreddy7 <thunderbotgamer199@gmail.com>
2026-06-29 22:38:30 +0530

    Simplify next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     unoptimized: true,
-  },
-  headers: async () => {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'X-DNS-Prefetch-Control',
-            value: 'on'
-          },
-        ],
-      },
-    ];
   },
 };
 
